6cb055d42a030869e295641ef046bf750fefb36d
anthonysea <anthonyqchao@gmail.com>
2020-03-17 18:33:18 -0700

    added visual cues to show that more content can be displayed

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Portfolio website
 
 ### To-do
-- Media query styling
+- ~~Media query styling~~
 - New tab/fullscreen when capture is clicked
+- ~~Added visual cues to show that more content can be displayed for each proejct~~
 - ~~About page + menu~~
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     $(".project-details").hide();
     $(".excerpt").click(function() {
-        $(this).siblings(".project-details").slideToggle("slow", function () {
-            //if ($(this).is(':visible'))
-              //  $(this).css('display', 'flex');
-        });
+        $(this).siblings(".project-details").slideToggle("slow", function () {});
+        $(this).children(".visual-cue").slideToggle("slow", function() {});
     });
+
+    $(".project-details").click(function() {
+        $(this).slideToggle("slow", function () {});
+        $(this).siblings(".excerpt").children(".visual-cue").slideToggle("slow", function() {});
+    });
+
     $(".about-me").hide();
     $(".site-title").click(function() {
         $(".about-me").slideToggle("slow", function() {});
